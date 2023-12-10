4dae90b81dd5b6388df7a9f6a7e34b75ba726adf
tbox1911 <tbox1911@gmail.com>
2023-12-10 17:39:52 +0100

    add ZW textures

--- LRX_Texture/config.cpp
+++ LRX_Texture/config.cpp
@@ -8,28 +8,92 @@ class CfgPatches
 		url = "https://forums.bohemia.net/forums/topic/217186-release-liberation-rx/";
 		weapons[] = {};
 		units[] = {};
-	};		
+	};
 };
 
 class LRX_TextureSources
 {
-	textures[] = {MyTextur01, MyTextur02, MyTextur03};
+	textures[] = {
+		MyTextur01,
+		MyTextur02,
+		MyTextur03,
+		ZW_Abstract_1,
+		ZW_Abstract_2,
+		ZW_Brown,
+		ZW_Desert,
+		ZW_Digital_Green,
+		ZW_Green,
+		ZW_old_metal,
+		ZW_Royalty,
+		ZW_scifi_camo_1,
+		ZW_scifi_camo_2,
+		ZW_Spot_camo,
+		ZW_Winter,
+		ZW_Woodland
+	};
 
-	class MyTextur01
-	{
+	class MyTextur01 {
 		name = "Hello Kitty";
 		texture = "LRX_Texture\textures\hellokitty.paa";
 	};
-
-	class MyTextur02
-	{
+	class MyTextur02 {
 		name = "TaZ";
 		texture = "LRX_Texture\textures\taz.paa";
 	};
-
-	class MyTextur03
-	{
+	class MyTextur03 {
 		name = "Raven";
 		texture = "LRX_Texture\textures\raven.paa";
-	};	
+	};
+	class ZW_Abstract_1 {
+		name = "ZW_Abstract_1";
+		texture = "LRX_Texture\textures\ZW_Abstract_1.paa";
+	};
+	class ZW_Abstract_2 {
+		name = "ZW_Abstract_2";
+		texture = "LRX_Texture\textures\ZW_Abstract_2.paa";
+	};
+	class ZW_Brown {
+		name = "ZW_Brown";
+		texture = "LRX_Texture\textures\ZW_Brown.paa";
+	};
+	class ZW_Desert {
+		name = "ZW_Desert";
+		texture = "LRX_Texture\textures\ZW_Desert.paa";
+	};
+	class ZW_Digital_Green {
+		name = "ZW_Digital_Green";
+		texture = "LRX_Texture\textures\ZW_Digital_Green.paa";
+	};
+	class ZW_Green {
+		name = "ZW_Green";
+		texture = "LRX_Texture\textures\ZW_Green.paa";
+	};
+	class ZW_old_metal {
+		name = "ZW_old_metal";
+		texture = "LRX_Texture\textures\ZW_old_metal.paa";
+	};
+	class ZW_Royalty {
+		name = "ZW_Royalty";
+		texture = "LRX_Texture\textures\ZW_Royalty.paa";
+	};
+	class ZW_scifi_camo_1 {
+		name = "ZW_scifi_camo_1";
+		texture = "LRX_Texture\textures\ZW_scifi_camo_1.paa";
+	};
+	class ZW_scifi_camo_2 {
+		name = "ZW_scifi_camo_2";
+		texture = "LRX_Texture\textures\ZW_scifi_camo_2.paa";
+	};
+	class ZW_Spot_camo {
+		name = "ZW_Spot_camo";
+		texture = "LRX_Texture\textures\ZW_Spot_camo.paa";
+	};
+	class ZW_Winter {
+		name = "ZW_Winter";
+		texture = "LRX_Texture\textures\ZW_Winter.paa";
+	};
+	class ZW_Woodland {
+		name = "ZW_Woodland";
+		texture = "LRX_Texture\textures\ZW_Woodland.paa";
+	};
 };
